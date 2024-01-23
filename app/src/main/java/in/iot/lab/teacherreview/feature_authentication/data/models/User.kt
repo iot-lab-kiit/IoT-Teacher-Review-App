7eb170af8f974124bb291cc857f40c8f27d7ff39
Vaibhav <vaibhavmonu8309@gmail.com>
2024-01-23 21:21:07 +0530

    fix typo in id

--- app/src/main/java/in/iot/lab/teacherreview/feature_authentication/data/models/User.kt
+++ app/src/main/java/in/iot/lab/teacherreview/feature_authentication/data/models/User.kt
@@ -4,7 +4,7 @@ package `in`.iot.lab.teacherreview.feature_authentication.data.models
  * This is the Structure of the User data which will be sent by the Database server and be stored
  * inside the Local Database
  *
- * @property id this is the unique ID for every user
+ * @property _id this is the unique ID for every user
  * @property name this contains the name of the User
  * @property email this is the email of the user from which the user logged in
  * @property uid this is the unique ID of the user from the Authentication Server
