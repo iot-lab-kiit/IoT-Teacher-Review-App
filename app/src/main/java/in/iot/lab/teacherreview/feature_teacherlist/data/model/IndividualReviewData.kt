f46d166e87b0f7c4cc93fe2718faf9656cec5218
Vaibhav <vaibhavmonu8309@gmail.com>
2024-01-26 03:35:21 +0530

    remove subject property from data classes

--- app/src/main/java/in/iot/lab/teacherreview/feature_teacherlist/data/model/IndividualReviewData.kt
+++ app/src/main/java/in/iot/lab/teacherreview/feature_teacherlist/data/model/IndividualReviewData.kt
@@ -9,7 +9,6 @@ import `in`.iot.lab.teacherreview.feature_authentication.data.models.User
  * @param review This is the review of the user
  * @param rating This contains the Rating Data of the particular Review
  * @param faculty This contains the data of the Faculty for which review is given
- * @param subject This is the Subject Data of the review
  * @param createdBy This contains the Data of the User who Created the Review
  * @param createdAt The time at which the review was created
  */
@@ -18,7 +17,6 @@ data class IndividualReviewData(
     val review: String? = null,
     val rating: RatingData? = null,
     val faculty: IndividualFacultyData,
-    val subject: SubjectsData,
     val createdBy: User,
     val createdAt : String
 )
