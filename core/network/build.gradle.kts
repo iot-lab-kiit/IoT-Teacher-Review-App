1f79eaa13b5da47f25c804ed91e3f30994e37d6e
Anirban Basak <jeetbasak2002@gmail.com>
2024-03-28 00:06:29 +0530

    refactor - Refactored the build.gradle of Auth Module.

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -48,9 +48,18 @@ dependencies {
     // Base Dependencies :----------------------------------------------------
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 
     //Retrofit 2.0 Dependency
     implementation(libs.retrofit)
