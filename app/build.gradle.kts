965ef6db036b5a76106d23f1db91ba680e4ef848
Vaibhav <68665948+dead8309@users.noreply.github.com>
2024-04-07 01:22:27 +0530

    BUMP minSdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         applicationId = "in.iot.lab.teacherreview"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
