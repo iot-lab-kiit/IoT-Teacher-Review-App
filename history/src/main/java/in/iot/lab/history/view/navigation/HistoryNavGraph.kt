2da547eeaa1ed1cfc4d2699b6d95e1894c5ecd94
Anirban Basak <jeetbasak2002@gmail.com>
2024-05-27 14:36:14 +0530

    update - Linked the Data Layer of History Module to Vm and UI Layer.

--- history/src/main/java/in/iot/lab/history/view/navigation/HistoryNavGraph.kt
+++ history/src/main/java/in/iot/lab/history/view/navigation/HistoryNavGraph.kt
@@ -1,8 +1,11 @@
 package `in`.iot.lab.history.view.navigation
 
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import `in`.iot.lab.history.view.screens.HistoryScreenControl
+import `in`.iot.lab.history.vm.HistoryViewModel
 
 
 const val HISTORY_ROUTE = "history-root-route"
@@ -10,6 +13,13 @@ const val HISTORY_ROUTE = "history-root-route"
 fun NavGraphBuilder.historyNavGraph() {
 
     composable(HISTORY_ROUTE) {
-        HistoryScreenControl()
+
+        val viewModel: HistoryViewModel = hiltViewModel()
+        val historyState = viewModel.historyState.collectAsState().value
+
+        HistoryScreenControl(
+            historyState = historyState,
+            setEvent = viewModel::uiListener
+        )
     }
 }
--- history/src/main/java/in/iot/lab/history/view/screens/HistoryScreen.kt
+++ history/src/main/java/in/iot/lab/history/view/screens/HistoryScreen.kt
@@ -1,13 +1,59 @@
 package `in`.iot.lab.history.view.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import `in`.iot.lab.design.components.AppFailureScreen
 import `in`.iot.lab.design.components.AppScreen
+import `in`.iot.lab.history.view.event.HistoryEvent
+import `in`.iot.lab.network.state.UiState
+import `in`.iot.lab.teacherreview.domain.models.review.RemoteReviewHistoryResponse
 
 
 @Composable
-fun HistoryScreenControl() {
-    AppScreen {
-        Text("History Screen")
+fun HistoryScreenControl(
+    historyState: UiState<List<RemoteReviewHistoryResponse>>,
+    setEvent: (HistoryEvent) -> Unit
+) {
+
+    LaunchedEffect(Unit) {
+        setEvent(HistoryEvent.FetchHistory)
     }
+
+    AppScreen {
+        when (historyState) {
+
+            is UiState.Idle -> {
+                setEvent(HistoryEvent.FetchHistory)
+            }
+
+            is UiState.Loading -> {
+                CircularProgressIndicator()
+            }
+
+            is UiState.Success -> {
+                HistorySuccessScreen(
+                    history = historyState.data,
+                    setEvent = setEvent
+                )
+            }
+
+            is UiState.Failed -> {
+                AppFailureScreen(
+                    text = historyState.message,
+                    onCancel = { },
+                    onTryAgain = { setEvent(HistoryEvent.FetchHistory) }
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun HistorySuccessScreen(
+    history: List<RemoteReviewHistoryResponse>,
+    setEvent: (HistoryEvent) -> Unit
+) {
+
 }
