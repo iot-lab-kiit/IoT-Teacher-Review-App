86ea1a7cb398518c30e656ff4259858bbeb2d96e
Anirban Basak <jeetbasak2002@gmail.com>
2024-05-27 14:06:32 +0530

    merge - Merged parts of review data layer to User Data layer.

--- data/src/main/java/in/iot/lab/teacherreview/data/repository/ReviewRepoImpl.kt
+++ data/src/main/java/in/iot/lab/teacherreview/data/repository/ReviewRepoImpl.kt
@@ -5,7 +5,6 @@ import `in`.iot.lab.network.utils.NetworkUtil.getResponseState
 import `in`.iot.lab.teacherreview.domain.models.review.RemoteReview
 import `in`.iot.lab.teacherreview.domain.models.review.RemoteFacultyReviewResponse
 import `in`.iot.lab.teacherreview.data.remote.ReviewApiService
-import `in`.iot.lab.teacherreview.domain.models.review.RemoteReviewHistoryResponse
 import `in`.iot.lab.teacherreview.domain.repository.ReviewRepo
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
--- data/src/main/java/in/iot/lab/teacherreview/domain/repository/ReviewRepo.kt
+++ data/src/main/java/in/iot/lab/teacherreview/domain/repository/ReviewRepo.kt
@@ -3,7 +3,6 @@ package `in`.iot.lab.teacherreview.domain.repository
 import `in`.iot.lab.network.state.ResponseState
 import `in`.iot.lab.teacherreview.domain.models.review.RemoteReview
 import `in`.iot.lab.teacherreview.domain.models.review.RemoteFacultyReviewResponse
-import `in`.iot.lab.teacherreview.domain.models.review.RemoteReviewHistoryResponse
 import kotlinx.coroutines.flow.Flow
 
 interface ReviewRepo {
