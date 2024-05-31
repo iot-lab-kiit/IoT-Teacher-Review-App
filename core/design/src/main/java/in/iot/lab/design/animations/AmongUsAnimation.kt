84717c9e99dcd6bf521727be75a62c1ad37e4e85
Kumar Sambhav <sambhav26k@gmail.com>
2024-05-31 21:09:07 +0530

    fix - LoadingAnimation

--- core/design/src/main/java/in/iot/lab/design/animations/AmongUsAnimation.kt
+++ core/design/src/main/java/in/iot/lab/design/animations/AmongUsAnimation.kt
@@ -1,6 +1,9 @@
 package `in`.iot.lab.design.animations
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -44,16 +47,39 @@ fun AmongUsAnimation(
 ) {
 
     // Animation Composition
-    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.among_us_animation))
-    val progress by animateLottieCompositionAsState(composition)
+    val composition1 by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.among_us_primary_animation))
+    val composition2 by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.among_us_secondary_animation))
+    val composition3 by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.among_us_tertiary_animation))
 
-    LottieAnimation(
-        composition = composition,
-        modifier = modifier.size(160.dp),
-        iterations = LottieConstants.IterateForever
-    )
+    val progress1 by animateLottieCompositionAsState(composition1)
+    val progress2 by animateLottieCompositionAsState(composition2)
+    val progress3 by animateLottieCompositionAsState(composition3)
+
+    Row(modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center) {
+        LottieAnimation(
+            composition = composition1,
+            modifier = modifier.size(160.dp)
+                .weight(1f),
+            iterations = LottieConstants.IterateForever
+        )
+
+        LottieAnimation(
+            composition = composition2,
+            modifier = modifier.size(160.dp)
+                .weight(1f),
+            iterations = LottieConstants.IterateForever
+        )
+
+        LottieAnimation(
+            composition = composition3,
+            modifier = modifier.size(160.dp)
+                .weight(1f),
+            iterations = LottieConstants.IterateForever
+        )
+    }
 
     onAnimationComplete?.let {
-        if (progress == 1.0f) it()
+        if (progress1 == 1.0f && progress2 == 1.0f && progress3 == 1.0f) it()
     }
 }
