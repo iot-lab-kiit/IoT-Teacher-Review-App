69a7a32db9a2978cfbb12f1e94b41867c91f9d6b
Soumyadeep Roy Karmakar <jrsoumya2k17@gmail.com>
2026-02-26 00:15:38 +0530

    fix- merge bug fix

--- review/src/main/java/in/iot/lab/review/view/screens/ReviewDetailScreen.kt
+++ review/src/main/java/in/iot/lab/review/view/screens/ReviewDetailScreen.kt
@@ -1,6 +1,7 @@
 package `in`.iot.lab.review.view.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
